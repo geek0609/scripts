119c911ef91a91dc01b44c84d64f8bfdf11f1ad2
Ashwin DS <astroashwin@outlook.com>
2022-03-12 23:39:19 +0400

    ubuntu-setup: install gh during setup

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -32,3 +32,11 @@ echo -e "Done."
 
 # Increase tmux scrollback buffer size
 echo "set-option -g history-limit 6000" >> .tmux.conf
+
+# GH
+echo "Installing GH........"
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+echo "Done Installing GH"
